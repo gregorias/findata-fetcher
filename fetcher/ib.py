7aadbc0fa9e4802e8f2bcc0e9b8e4057525d4dcb
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-11-27 23:33:20 +0100

    Add IB CSV fetching

--- fetcher/ib.py
+++ fetcher/ib.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 # TODO clean up and test up
 """Fetches account statement data from Interactive Brokers"""
+import base64
+import json
 from typing import NamedTuple
 from selenium import webdriver  # type: ignore
 from selenium.webdriver.common.by import By  # type: ignore
 from selenium.webdriver.common.keys import Keys  # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
 from selenium.webdriver.support import expected_conditions  # type: ignore
+import requests
+
+from .driverutils import format_date, driver_cookie_jar_to_requests_cookies
 
 
 class Credentials(NamedTuple):
@@ -62,6 +67,36 @@ def fetch_all_transactions_since_2018(
     mtm_summary_box.find_element(By.CSS_SELECTOR,
                                  '.fa-arrow-circle-right').click()
     return use_the_modal_box_to_download_the_csv(driver)
+
+
+def fetch_account_statement_csv(
+        driver: webdriver.remote.webdriver.WebDriver) -> bytes:
+    fetch_url = 'https://www.interactivebrokers.co.uk/AccountManagement/Statements/Run'
+    payload = {
+        'format': 13,
+        'fromDate': '20201126',
+        'reportDate': '20201126',
+        'toDate': '20201126',
+        'language': 'en',
+        'period': 'DATE_RANGE',
+        'statementCategory': 'DEFAULT_STATEMENT',
+        'statementType': 'MTM_SUMMARY'
+    }
+    cookies = driver_cookie_jar_to_requests_cookies(driver.get_cookies())
+    am_session_id = driver.execute_script('return AM_SESSION_ID;')
+    headers = {
+        'SessionId': am_session_id,
+    }
+    response = requests.get(
+        fetch_url,
+        params=payload,  # type: ignore
+        cookies=cookies,
+        headers=headers)
+    if not response.ok:
+        raise Exception("The statement fetch request has failed. " +
+                        ('Response reason: {0}, parameters: {1}'
+                         ).format(response.reason, (fetch_url, cookies)))
+    return base64.decodebytes(json.loads(response.content)['fileContent'])
 
 
 # https://www.interactivebrokers.co.uk/AccountManagement/Statements/Run?format=13&fromDate=20200428&language=en&period=DATE_RANGE&reportDate=20201125&statementCategory=DEFAULT_STATEMENT&statementType=MTM_SUMMARY&toDate=20201125
