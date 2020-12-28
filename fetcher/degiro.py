05586ba9c1e43ced398cbfdf2fc8d06f2fa6d347
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-12-28 13:29:38 +0100

    Add the basic structure for Degiro fetching

--- fetcher/degiro.py
+++ fetcher/degiro.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Fetches account statement from Degiro"""
 from typing import NamedTuple
 import logging
@@ -7,6 +8,9 @@ from selenium.webdriver.common.by import By  # type: ignore
 from selenium.webdriver.common.keys import Keys  # type: ignore
 from selenium.webdriver.support import expected_conditions  # type: ignore
 from selenium.webdriver.support.ui import WebDriverWait  # type: ignore
+import requests
+
+from .driverutils import driver_cookie_jar_to_requests_cookies
 
 
 class Credentials(NamedTuple):
@@ -16,13 +20,45 @@ class Credentials(NamedTuple):
 
 def login(creds: Credentials,
           driver: webdriver.remote.webdriver.WebDriver) -> None:
+    logging.info("Logging in.")
     LOGIN_PAGE = 'https://trader.degiro.nl/login/chde/#/login'
     driver.get(LOGIN_PAGE)
-    wait = WebDriverWait(driver, 30)
-    wait.until(
-        expected_conditions.presence_of_element_located((By.ID, 'username')))
     driver.find_element(By.ID, "username").send_keys(creds.id + Keys.TAB)
     driver.find_element(By.ID, "password").send_keys(creds.pwd + Keys.RETURN)
+
+
+def wait_for_login(driver: webdriver.remote.webdriver.WebDriver):
+    logging.info("Waiting for login.")
+    wait = WebDriverWait(driver, 30)
+    wait.until(
+        expected_conditions.url_to_be(
+            'https://trader.degiro.nl/trader/#/markets'))
+
+
+def fetch_csv(url: str, cookies) -> bytes:
+    response = requests.post(url, cookies=cookies)
+    if not response.ok:
+        raise Exception("The URL fetch request has failed. " +
+                        ('Response reason: {0}, parameters: {1}'
+                         ).format(response.reason, (url, cookies)))
+    return response.content
+
+
+def fetch_portfolio(driver: webdriver.remote.webdriver.WebDriver) -> bytes:
+    logging.info("Fetching portfolio.")
+    portfolioSideBarLink = driver.find_element(By.CSS_SELECTOR,
+                                               '[href="#/portfolio"]')
+    portfolioSideBarLink.click()
+    exportButton = driver.find_element(By.CSS_SELECTOR,
+                                       '[data-name="exportButton"]')
+    exportButton.click()
+    reportExportForm = driver.find_element(By.CSS_SELECTOR,
+                                           '[data-name="reportExportForm"]')
+    csvLink = reportExportForm.find_element(
+        By.XPATH, "//a[normalize-space(text()) = 'CSV']")
+    csvFetchUrl = csvLink.get_attribute('href')
+    cookies = driver_cookie_jar_to_requests_cookies(driver.get_cookies())
+    return fetch_csv(csvFetchUrl, cookies)
 
 
 def fetch_statement(creds: Credentials) -> bytes:
@@ -32,8 +68,7 @@ def fetch_statement(creds: Credentials) -> bytes:
         A CSV UTF-8 encoded statement.
     """
     with webdriver.Firefox() as driver:
-        logging.info("Logging in.")
+        driver.implicitly_wait(30)
         login(creds, driver)
-        logging.info("Waiting for login.")
-        logging.info("Fetching transaction data.")
-        return b''
+        wait_for_login(driver)
+        return fetch_portfolio(driver)
